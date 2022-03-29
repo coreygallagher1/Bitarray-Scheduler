16cbbabd99d8fdc4fee39330390418403081ff8f
Corey Gallagher <coreygallagher1194@gmail.com>
2022-03-29 15:13:02 -0500

    corrected the bitmap and bitarray
    bit array still doesn't print right when called through the printbitarraymap

--- bitmaps.c
+++ bitmaps.c
@@ -1,23 +1,46 @@
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "scheduler.h"
+//#include "scheduler.h"
+#include "print-utils.c"
 
 // TODO
 bit_array createBitArray (int times, int position) {
-  // implement the function and replace the return line below
+  char a[5] = {1, 3, 7, 15, 31};
+  bit_array bitArr;
+  
+  if(times + position > 5){
+    bitArr = 0;
+  }
+
+  else{
+    bitArr = a[times-1] << position;
+  }
+
+  return bitArr;
+
   return 0;
 }
 
 // TODO
 bitmap * createBitmap (int times, int id) {
-  // implement the function and replace the return line below
-  return NULL;
+  bitmap bitMap;
+  bitMap.offer_id = id;
+  
+    for(int i = 0; i < 5; i++){
+      printBinary(createBitArray(times, i));
+      bitMap.b[i] = createBitArray(times, i);
+      bitMap.next;
+      
+
+    }
+    bitMap.next = &bitMap;
+    return &bitMap;
 }
 
 int main () {
-
   bitmap * m = createBitmap(1, 0);
+
   printBitmap(m);
   m = createBitmap(2, 1);
   printBitmap(m);
